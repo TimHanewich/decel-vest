2210c1f6d1ef5aaa7ce1c93d3e57b1b4c72f13e0
Tim Hanewich <tahanewich@live.com>
2022-07-09 16:39:42 -0400

    Fixed attitude math (I made an error by swapping)

--- attitude_math.py
+++ attitude_math.py
@@ -10,9 +10,8 @@ def attitude(gx:float,gy:float,gz:float):
 
     # calculate in radions
     # I'm taking this calculation from here (page 3): https://aatishb.com/materials/srr/workshop3.pdf
-    # but it seems they have flipped the pitch and roll calculations. So I swapped them.
-    pitch_rads = math.atan(ms2_y / math.sqrt((ms2_x * ms2_x) + (ms2_z * ms2_z)))
-    roll_rads = math.atan(ms2_x / math.sqrt((ms2_y * ms2_y) + (ms2_z * ms2_z)))
+    pitch_rads = math.atan(ms2_x / math.sqrt((ms2_y * ms2_y) + (ms2_z * ms2_z)))
+    roll_rads = math.atan(ms2_y / math.sqrt((ms2_x * ms2_x) + (ms2_z * ms2_z)))
     
     # convert from rads to degrees
     pitch_degs = pitch_rads * (180 / math.pi)
