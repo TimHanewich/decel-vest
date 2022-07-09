6570f40be0625bd6ff01a5e321f214e5ed64ab78
Tim Hanewich <tahanewich@live.com>
2022-07-09 17:07:26 -0400

    Added dlogging module

---
+++ dlogging.py
@@ -0,0 +1,12 @@
+def log(txt:str):
+    print(txt)
+    f = open("log.txt", "a")
+    f.write(txt)
+    f.close()
+
+def retrieve_logs():
+    try:
+        f = open("log.txt", "r")
+        return f.read()
+    except:
+        return ""
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from imu import MPU6050
 import time
 from machine import Pin, I2C
 import attitude_math
+import dlogging
+
+# log that I am turned on
+dlogging.log("Turned on!")
 
 i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
 imu = MPU6050(i2c)
@@ -14,18 +18,18 @@ while True:
     ax=round(imu.accel.x,2)
     ay=round(imu.accel.y,2)
     az=round(imu.accel.z,2)
-    print("Values acquired")
+    dlogging.log("Got values")
 
     try:
-        print("Calculating attitude")
+        dlogging.log("Calculating attitude")
         attitude = attitude_math.attitude(ax, ay, az)
         pitch = round(attitude[0], 0)
         roll = round(attitude[1], 0)
-        print("pitch: " + str(pitch))
-        print("roll: " + str(roll))
-        print(str(pitch) + " " + str(roll))
+        dlogging.log("pitch: " + str(pitch))
+        dlogging.log("roll: " + str(roll))
+        dlogging.log(str(pitch) + " " + str(roll))
     except Exception as e:
-        print("ERROR: " + e)
+        dlogging.log("ERROR: " + e)
 
     
     # Following sleep statement makes values enought stable to be seen and
