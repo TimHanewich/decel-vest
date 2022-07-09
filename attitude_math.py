4b34ab902272296bb20f96a3a5b2b50ee4bf2362
Tim Hanewich <tahanewich@live.com>
2022-07-09 17:21:11 -0400

    Attitude calculations checks for 0 values

--- attitude_math.py
+++ attitude_math.py
@@ -10,11 +10,24 @@ def attitude(gx:float,gy:float,gz:float):
 
     # calculate in radions
     # I'm taking this calculation from here (page 3): https://aatishb.com/materials/srr/workshop3.pdf
-    pitch_rads = math.atan(ms2_x / math.sqrt((ms2_y * ms2_y) + (ms2_z * ms2_z)))
-    roll_rads = math.atan(ms2_y / math.sqrt((ms2_x * ms2_x) + (ms2_z * ms2_z)))
+
+    # calculate pitch
+    tdb = math.sqrt((ms2_y * ms2_y) + (ms2_z * ms2_z))
+    if tdb == 0:
+        return 0,0
+    pitch_rads = math.atan(ms2_x / tdb)
+
+    # calculate roll
+    tdb = math.sqrt((ms2_x * ms2_x) + (ms2_z * ms2_z))
+    if tdb == 0:
+        return 0,0
+    roll_rads = math.atan(ms2_y / tdb)
     
     # convert from rads to degrees
     pitch_degs = pitch_rads * (180 / math.pi)
     roll_degs = roll_rads * (180 / math.pi)
 
     return pitch_degs, roll_degs
+
+
+print(attitude(0, 0, 0))
