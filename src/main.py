1cd58b96daa07700e846ec3aacf69edecbc3ad56
Tim Hanewich <tahanewich@live.com>
2022-07-10 15:38:02 -0400

    Placed read in try

--- src/main.py
+++ src/main.py
@@ -25,12 +25,17 @@ imu = MPU6050(i2c)
 
 while True:
 
-    ax=round(imu.accel.x,2)
-    ay=round(imu.accel.y,2)
-    az=round(imu.accel.z,2)
-    tem=round(imu.temperature,2)
+    try:
 
-    # Log it
-    dlogging.log(str(ax) + "_" + str(ay) + "_" + str(az) + "_" + str(tem))
+        ax=round(imu.accel.x,2)
+        ay=round(imu.accel.y,2)
+        az=round(imu.accel.z,2)
+        tem=round(imu.temperature,2)
+
+        # Log it
+        dlogging.log(str(ax) + "_" + str(ay) + "_" + str(az) + "_" + str(tem))
+
+    except:
+        dlogging.log("Critical failure while reading!")
 
     time.sleep(0.5)
