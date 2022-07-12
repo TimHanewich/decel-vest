951ce8e99c915266f3da453efc984d0a4a1534a4
Tim Hanewich <tahanewich@live.com>
2022-07-12 13:45:19 -0400

    Moved max and min hz to settings file

--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,3 @@
 gpio_strobe = 1
+max_hz = 15.0
+min_hz = 1.5
--- src/strobe_tools.py
+++ src/strobe_tools.py
@@ -1,17 +1,14 @@
-
-# Settings
-max_hz = 20.0
-min_hz = 2.0
+import settings
 
 # percentage supplied between 0.0 and 1.0
 def select_hertz(percentage:float) -> float:
     if percentage < 0.0:
-        return min_hz
+        return settings.min_hz
     elif percentage > 1.0:
-        return max_hz
+        return settings.max_hz
 
-    to_add = (max_hz - min_hz) * percentage
-    to_return = min_hz + to_add
+    to_add = (settings.max_hz - settings.min_hz) * percentage
+    to_return = settings.min_hz + to_add
     return to_return
 
 # calculates the number of seconds in between each pulse
