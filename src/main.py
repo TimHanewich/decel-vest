97a6d91cefbf18a2d7fab1878a1b90e1677b1d77
Tim Hanewich <tahanewich@live.com>
2022-07-12 14:09:27 -0400

    Wrote program to interpret pitch into hz

--- src/main.py
+++ src/main.py
@@ -1,50 +1,35 @@
 import strobe_controller
 import _thread
 import time
-import dlogging
 import settings
+from imu import MPU6050
+from machine import Pin, I2C
+import attitude_math
+import math
+import strobe_tools
+
 
 _thread.start_new_thread(strobe_controller.continuous_strobe, ())
-time.sleep(10)
 
-for hz in range(settings.min_hz, settings.max_hz):
+
+# Turn on the status LED
+led = Pin(25, Pin.OUT)
+led.value(1)
+
+i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
+imu = MPU6050(i2c)
+
+while True:
+
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    
+    attitude = attitude_math.attitude(ax, ay, az)
+    pitch = abs(attitude[0])
+    pitch_percentage = pitch / 90
+    
+    hz = strobe_tools.select_hertz(pitch_percentage)
     strobe_controller.set_hertz(hz)
-    time.sleep(1)
 
-time.sleep(5)
-strobe_controller.mute()
-time.sleep(5)
-strobe_controller.set_hertz(5)
-strobe_controller.unmute()
-
-# from imu import MPU6050
-# import time
-# from machine import Pin, I2C
-# import dlogging
-
-# # log an "I'm on!" message
-# dlogging.log("main.py started! Hello world!")
-
-# # Turn on the status LED
-# led = Pin(25, Pin.OUT)
-# led.value(1)
-
-# i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
-# imu = MPU6050(i2c)
-
-# while True:
-
-#     try:
-
-#         ax=round(imu.accel.x,2)
-#         ay=round(imu.accel.y,2)
-#         az=round(imu.accel.z,2)
-#         tem=round(imu.temperature,2)
-
-#         # Log it
-#         dlogging.log(str(ax) + "_" + str(ay) + "_" + str(az) + "_" + str(tem))
-
-#     except:
-#         dlogging.log("Critical failure while reading!")
-
-#     time.sleep(0.5)
+    time.sleep(0.5)
