3df35b6797eed54f8e19c77d579e29f73f349297
Tim Hanewich <tahanewich@live.com>
2022-07-12 09:38:19 -0400

    Wrote program

--- extract/Program.cs
+++ extract/Program.cs
@@ -7,9 +7,10 @@ namespace extract
     {
         public static void Main(string[] args)
         {
-            string content = System.IO.File.ReadAllText(@"C:\Users\tahan\Downloads\20220711 Motorcycle Ride\log_adj.txt");
+            string content = System.IO.File.ReadAllText(@"C:\Users\tahan\Downloads\20220711 Calibration Test 2\log.txt");
             MPU6050Log[] logs = MPU6050Log.Extract(content);
-            Console.WriteLine(JsonSerializer.Serialize(logs));
+            string csv = MPU6050Log.ToCSV(logs);
+            System.IO.File.WriteAllText(@"C:\Users\tahan\Downloads\20220711 Calibration Test 2\log.csv", csv);
         }
     }
 }
