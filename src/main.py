b02b8bbf6d55888bc70cb88e6940d73c8a7a81c3
Tim Hanewich <tahanewich@live.com>
2022-07-13 12:23:56 -0400

    Placed read buff in try

--- src/main.py
+++ src/main.py
@@ -11,12 +11,12 @@ gpsModule = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
 dlogging.log("Ready to go!")
 
 while True:
-    buff = gpsModule.readline()
     try:
+        buff = gpsModule.readline()
         nmea.parse(buff)
         dlogging.log(str(nmea.latitude) + "," + str(nmea.longitude) + " - " + str(nmea.satellites) + " on " + buff.decode())
     except:
-        pass
+        dlogging.log("Failed!")
 
 
 
