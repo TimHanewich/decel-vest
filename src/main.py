8c215437003bf13d8139986faaa11bdcad7b4253
Tim Hanewich <tahanewich@live.com>
2022-07-13 18:25:41 -0400

    Re-wrote main to use new methods

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ from machine import Pin, UART
 import time
 import dlogging
 import nmea
+import speed_controller
+
 
 # Turn on the LED
 led = Pin(25, Pin.OUT)
 led.value(1)
 
 gpsModule = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+sc = speed_controller.speed_controller()
 
 while True:
 
@@ -33,44 +36,16 @@ while True:
             if "*" in line:
                 line_collected = True
 
-    # now that the line is collected, parse it
+    # now that the line is collected, parse it into GPS telemetry
+    tele = None
     try:
-       nmea.parse(line)
-       dlogging.log(str(nmea.latitude) + "," + str(nmea.longitude) + " = " + str(nmea.speed_mph))
-    except Exception as e:
-       pass
+        tele = nmea.parse(line)
+    except:
+        tele = None
 
+    # if it parsed into a gps telemetry, feed it
+    if tele != None:
+        sc.ingest(tele)
 
-# import strobe_controller
-# import _thread
-# import time
-# import settings
-# from imu import MPU6050
-# from machine import Pin, I2C
-# import attitude_math
-# import math
-# import strobe_tools
-
-# _thread.start_new_thread(strobe_controller.continuous_strobe, ())
-
-# # Turn on the status LED
-# led = Pin(25, Pin.OUT)
-# led.value(1)
-
-# i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
-# imu = MPU6050(i2c)
-
-# while True:
-
-#     ax=round(imu.accel.x,2)
-#     ay=round(imu.accel.y,2)
-#     az=round(imu.accel.z,2)
-    
-#     attitude = attitude_math.attitude(ax, ay, az)
-#     pitch = abs(attitude[0])
-#     pitch_percentage = pitch / 90
-    
-#     hz = strobe_tools.select_hertz(pitch_percentage)
-#     strobe_controller.set_hertz(hz)
-
-#     time.sleep(0.5)
+    # save
+    dlogging.log("MPH: " + str(sc.speed_mph) + "   MPH/S: " + str(sc.acceleration_mphs))
