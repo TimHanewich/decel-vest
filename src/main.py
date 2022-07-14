4d7d1e142ce886fece981f460cedd2902904876b
Tim Hanewich <tahanewich@live.com>
2022-07-14 14:47:03 -0400

    Adjusted logging

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import nmea
 import _thread
 from machine import Pin
 import speed_controller
+import dlogging
 
 # Turn on the LED
 led = Pin(25, Pin.OUT)
@@ -11,6 +12,8 @@ led.value(1)
 # create the variables that we will use
 sc = speed_controller.speed_controller()
 
+# print the headers
+dlogging.log("fix_speed_lat_lon_sats", False)
 
 # write the function that will handle the receiving of gps
 def process_gps_tele(tele:nmea.gps_telemetry):
@@ -19,7 +22,7 @@ def process_gps_tele(tele:nmea.gps_telemetry):
         if tele.fixed != None and tele.latitude != None and tele.longitude != None:
             sc.ingest(tele.fixed, tele.latitude, tele.longitude)
     
-    print(str(tele.fixed) + " = " + str(sc.speed_mph))
+    dlogging.log(str(tele.fixed) + "_" + str(sc.speed_mph) + "_" + str(tele.latitude) + "_" + str(tele.longitude) + "_" + str(tele.satellites))
 
 # start the gps driver
 gps = gps_driver.gps_driver()
