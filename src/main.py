9e6c2e8d3bca2d01ba111c6088cdf7aa57e3447c
Tim Hanewich <tahanewich@live.com>
2022-07-14 14:33:58 -0400

    Rewrote process gps function

--- src/main.py
+++ src/main.py
@@ -2,17 +2,26 @@ import gps_driver
 import nmea
 import _thread
 from machine import Pin
+import speed_controller
 
 # Turn on the LED
 led = Pin(25, Pin.OUT)
 led.value(1)
 
+# create the variables that we will use
+sc = speed_controller.speed_controller()
+
 
 # write the function that will handle the receiving of gps
-def print_tele(tele:nmea.gps_telemetry):
-    print(str(tele.fixed) + " - " + str(tele.latitude) + "," + str(tele.longitude))
+def process_gps_tele(tele:nmea.gps_telemetry):
+    global sc
+    if tele != None:
+        if tele.fixed != None and tele.latitude != None and tele.longitude != None:
+            sc.ingest(tele.fixed, tele.latitude, tele.longitude)
+    
+    print(str(tele.fixed) + " = " + str(sc.speed_mph))
 
 # start the gps driver
 gps = gps_driver.gps_driver()
-gps.set_callback(print_tele)
+gps.set_callback(process_gps_tele)
 _thread.start_new_thread(gps.start(), ())
