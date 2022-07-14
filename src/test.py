0f116695a2da871484c9147e82558cf78aded596
Tim Hanewich <tahanewich@live.com>
2022-07-14 19:37:04 -0400

    Tweaked test

--- src/test.py
+++ src/test.py
@@ -11,6 +11,7 @@ led.value(1)
 
 sc = strobe_calculator.strobe_calculator()
 _thread.start_new_thread(strobe_controller.continuous_strobe, ())
+strobe_controller.mute()
 
 print("Opening file...")
 f = open(r"/example-data.csv", "r")
@@ -19,12 +20,17 @@ content = f.readlines()
 print("Here we go!")
 for line in content:
     if "fix" not in line:
+
+        
+
         row = line.split(",")
         fix = int(row[0])
         speed = float(row[1])
         lat = float(row[2])
         lon = float(row[3])
         sats = float(row[4])
+
+        print("Now on: " + str(fix))
         
         hz = sc.ingest(fix, speed)
         if hz != None:
