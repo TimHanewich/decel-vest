ddcbf54b36fee091975e44d2ee1e5d16ce9d7c67
Tim Hanewich <tahanewich@live.com>
2022-07-14 19:10:36 -0400

    Lifted max decel

--- src/settings.py
+++ src/settings.py
@@ -5,4 +5,4 @@ min_hz = 1
 
 # Deceleration Settings - in MPH/S. For example, 1 would mean losing 1 MPH per second (decelerating)
 min_decel = 1.0 # the minimum deceleration that has to occur for it to be registered as a deceleration and thus trigger a blink
-max_decel = 2.2 # the maximum deceleration we would expect to see. Used to calculate the hz of the light (our maximum)
+max_decel = 3.0 # the maximum deceleration we would expect to see. Used to calculate the hz of the light (our maximum)
