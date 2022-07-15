85aade8eea0958a25dfa9af31b6da57d5bd259fa
Tim Hanewich <tahanewich@live.com>
2022-07-15 13:53:01 -0400

    Fixed strobe calculator

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,9 @@ max_hz = 15
 min_hz = 1
 
 # Deceleration Settings - in MPH/S. For example, 1 would mean losing 1 MPH per second (decelerating)
-min_decel = 1.0 # the minimum deceleration that has to occur for it to be registered as a deceleration and thus trigger a blink
-max_decel = 3.0 # the maximum deceleration we would expect to see. Used to calculate the hz of the light (our maximum)
+min_decel_trigger = 1.0 # the minimum deceleration that has to occur for it to be registered as a deceleration and thus enter in decelerating mode
+min_decel_sustained = 0.3 #when deceleration is first detected, this is the minimum deceleration that has to be sustained in order for deceleration to be sustained. The moment deceleration drops below this, we move to the "awaiting deceleration" mode.
+
+# Deceleration settings for calculating hz - these are used to calcualte where we are in the range and thus calculate a hz for the strobe
+min_decel_strobe = 0.3
+max_decel_strobe = 3.0
--- src/strobe_calculator.py
+++ src/strobe_calculator.py
@@ -31,11 +31,11 @@ class strobe_calculator:
 
             # calculate acceleration/deceleration - acceleration would be positie, deceleration would be negative
             accel_mphs = (speed_mph - old_speed) / (fixed - old_fixed)
-            print("Accel: " + str(accel_mphs) + " from " + str(old_speed) + " to " + str(speed_mph))
+            print(str(fixed) + " - " + "Accel: " + str(accel_mphs) + " from " + str(old_speed) + " to " + str(speed_mph))
 
             # if we are currently in decelerating mode, check to see if it is over. If it is over, return none
             if self.__mode__ == Decelerating: # we are currently decelerating
-                if accel_mphs > (settings.min_decel * -1): # if we are no longer decelerating in the window
+                if accel_mphs > (settings.min_decel_sustained * -1): # if we are no longer decelerating in the window
 
                     # mark the status as awaiting deceleration (neutral)
                     self.__mode__ = AwaitingDeceleration
@@ -49,7 +49,7 @@ class strobe_calculator:
                     if old_speed > 30: # only consider decelerations from above this MPH
 
                         # only continue if deceleration is greater than than a certain amount
-                        if accel_mphs <= (settings.min_decel * -1): # if we are losing more than this many MPH per second 
+                        if accel_mphs <= (settings.min_decel_trigger * -1): # if we are losing more than this many MPH per second 
                             
                             # mark the status as decelerating
                             self.__mode__ = Decelerating
@@ -58,7 +58,7 @@ class strobe_calculator:
             # if we are currently decelerating, calculate the hz of the strobe and return it
             if self.__mode__ == Decelerating:
                 # turn it into a percentage that we will use to calculate the hz of the strobe
-                decel_percent = (abs(accel_mphs) - settings.min_decel) / (settings.max_decel - settings.min_decel)
+                decel_percent = (abs(accel_mphs) - settings.min_decel_trigger) / (settings.max_decel_strobe - settings.min_decel_strobe)
                 decel_percent = min(decel_percent, 1.0) #make sure it doesn't exceed 100%
                 decel_percent = max(decel_percent, 0) #make sure it doesn't fall below 0%
                 hz = strobe_tools.select_hertz(decel_percent) # CALCULATING THE VALUE TO RETURN
