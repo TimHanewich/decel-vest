80bbde8aa631ee5d1e5bd2ef7bf5be9a2aa8da93
Tim Hanewich <tahanewich@live.com>
2022-07-15 23:58:01 -0400

    Placed entire process function in a try brcket

--- src/main.py
+++ src/main.py
@@ -24,27 +24,28 @@ dlogging.log("fix_speed_lat_lon_sats_hz", False)
 
 # write the function that will handle the receiving of gps
 def process_gps_tele(tele:nmea.gps_telemetry):
+    try:
+        global sc
+        global strobe_calc
 
-    global sc
-    global strobe_calc
+        if tele != None:
+            if tele.fixed != None and tele.latitude != None and tele.longitude != None:
+                
+                # calculate  the speed
+                sc.ingest(tele.fixed, tele.latitude, tele.longitude) #ingest the data to get the speed
+        
+                # calculate the appropriate hertz of the light
+                hz = strobe_calc.ingest(tele.fixed, sc.speed_mph)
+                if hz != None:
+                    strobe_controller.set_hertz(hz)
+                    strobe_controller.unmute()
+                else:
+                    strobe_controller.mute()
 
-    if tele != None:
-        if tele.fixed != None and tele.latitude != None and tele.longitude != None:
-            
-            # calculate  the speed
-            sc.ingest(tele.fixed, tele.latitude, tele.longitude) #ingest the data to get the speed
-     
-            # calculate the appropriate hertz of the light
-            hz = strobe_calc.ingest(tele.fixed, sc.speed_mph)
-            if hz != None:
-                strobe_controller.set_hertz(hz)
-                strobe_controller.unmute()
-            else:
-                strobe_controller.mute()
-
-            # log the data
-            dlogging.log(str(tele.fixed) + "_" + str(sc.speed_mph) + "_" + str(tele.latitude) + "_" + str(tele.longitude) + "_" + str(tele.satellites) + "_" + str(hz), False)
-
+                # log the data
+                dlogging.log(str(tele.fixed) + "_" + str(sc.speed_mph) + "_" + str(tele.latitude) + "_" + str(tele.longitude) + "_" + str(tele.satellites) + "_" + str(hz), False)
+    except Exception as e:
+        dlogging.log("Critical error! Msg: " + str(e))
 
 # start the gps driver
 gps = gps_driver.gps_driver()
