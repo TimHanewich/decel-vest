74e2940548e20262503ddc717684739798f699c6
Tim Hanewich <tahanewich@live.com>
2022-07-17 16:54:04 -0400

    Removed try bracket

--- src/gps_driver.py
+++ src/gps_driver.py
@@ -53,52 +53,45 @@ class gps_driver:
         
 
     def get_telemetry(self, timeout_ms:int = 5000) -> nmea.gps_telemetry:
-
-        try:
-            
-
-            # if the gpsModule hasn't been set up, return none
-            if self.__gpsModule__ == None:
-                return None
-
-            # get the to return object
-            ToReturn = None
-            StartedSearchAt = time.ticks_ms()
-            while ToReturn == None and (time.ticks_ms() - StartedSearchAt) < timeout_ms:
-                # collect line
-                line = ""
-                line_collected = False
-                while line_collected == False:
-                    tidbit = self.__gpsModule__.readline()
-                    if tidbit != None:
-
-                        # Try to decode
-                        tidbit_txt = None
-                        try:
-                            tidbit_txt = tidbit.decode()
-                        except:
-                            tidbit_txt = None
-                        
-                        # if it decoded successfully, ad it to the line
-                        if tidbit_txt != None:
-                            line = line + tidbit_txt
-                            
-                        # is it now fully collected?
-                        if "*" in line:
-                            line_collected = True
-
-                # now that the line is collected, parse it into GPS telemetry
-                tele = None
-                try:
-                    tele = nmea.parse(line)
-                except:
-                    tele = None
-
-                # if it parsed into a gps telemetry, feed it
-                if tele != None:
-                    ToReturn = tele
-                
-            return ToReturn
-        except Exception as e:
-            print("Error while getting tele: " + str(e))
+        # if the gpsModule hasn't been set up, return none
+        if self.__gpsModule__ == None:
             return None
+
+        # get the to return object
+        ToReturn = None
+        StartedSearchAt = time.ticks_ms()
+        while ToReturn == None and (time.ticks_ms() - StartedSearchAt) < timeout_ms:
+            # collect line
+            line = ""
+            line_collected = False
+            while line_collected == False:
+                tidbit = self.__gpsModule__.readline()
+                if tidbit != None:
+
+                    # Try to decode
+                    tidbit_txt = None
+                    try:
+                        tidbit_txt = tidbit.decode()
+                    except:
+                        tidbit_txt = None
+                    
+                    # if it decoded successfully, ad it to the line
+                    if tidbit_txt != None:
+                        line = line + tidbit_txt
+                        
+                    # is it now fully collected?
+                    if "*" in line:
+                        line_collected = True
+
+            # now that the line is collected, parse it into GPS telemetry
+            tele = None
+            try:
+                tele = nmea.parse(line)
+            except:
+                tele = None
+
+            # if it parsed into a gps telemetry, feed it
+            if tele != None:
+                ToReturn = tele
+            
+        return ToReturn
