5666db875d8b564d53824155c3ff2d06154e9da5
Tim Hanewich <tahanewich@live.com>
2022-07-17 17:02:28 -0400

    Added raw recording mode

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 decel_vest = 0
 strobe_test = 1
 record_processed = 2
+record_raw = 3
 
 # RUN MODE
 EXECUTION_MODE = strobe_test
@@ -12,5 +13,7 @@ elif EXECUTION_MODE == strobe_test:
     import strobe_test
 elif EXECUTION_MODE == record_processed:
     import record_processed
+elif EXECUTION_MODE == record_raw:
+    import record_raw
 else:
     print("Did not understand execution mode '" + str(EXECUTION_MODE) + "'")
