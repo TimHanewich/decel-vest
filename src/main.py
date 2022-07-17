fb6711c7907b59ebebee5c852aef45788e4bd6af
Tim Hanewich <tahanewich@live.com>
2022-07-17 12:25:33 -0400

    Record processed can be executed by main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # Available Mode
 decel_vest = 0
 strobe_test = 1
+record_processed = 2
 
 # RUN MODE
 EXECUTION_MODE = strobe_test
@@ -9,5 +10,7 @@ if EXECUTION_MODE == decel_vest:
     import decel_vest
 elif EXECUTION_MODE == strobe_test:
     import strobe_test
+elif EXECUTION_MODE == record_processed:
+    import record_processed
 else:
     print("Did not understand execution mode '" + str(EXECUTION_MODE) + "'")
