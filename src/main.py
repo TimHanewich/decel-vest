7aca7a377924d0ff291627074669a9f2d72daa17
Tim Hanewich <tahanewich@live.com>
2022-07-18 15:04:58 -0400

    Added strobe solid

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ decel_vest = 0
 strobe_test = 1
 record_processed = 2
 record_raw = 3
+strobe_solid = 4
 
 # RUN MODE
 EXECUTION_MODE = strobe_test
@@ -15,5 +16,7 @@ elif EXECUTION_MODE == record_processed:
     import record_processed
 elif EXECUTION_MODE == record_raw:
     import record_raw
+elif EXECUTION_MODE == strobe_solid:
+    import strobe_solid
 else:
     print("Did not understand execution mode '" + str(EXECUTION_MODE) + "'")
