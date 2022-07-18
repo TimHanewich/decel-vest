cd4f5086d7ed9c7d6cf7bcac85d70b36cef3880d
Tim Hanewich <tahanewich@live.com>
2022-07-18 17:44:22 -0400

    Boundary test

--- extract/Program.cs
+++ extract/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Text.Json;
+using System.Collections.Generic;
+using extract.Boundary;
 
 namespace extract
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            string content = System.IO.File.ReadAllText(@"C:\Users\tahan\Downloads\20220711 Calibration Test 2\log.txt");
-            MPU6050Log[] logs = MPU6050Log.Extract(content);
-            string csv = MPU6050Log.ToCSV(logs);
-            System.IO.File.WriteAllText(@"C:\Users\tahan\Downloads\20220711 Calibration Test 2\log.csv", csv);
+            Point p1 = new Point();
+            p1.X = 27.207793f;
+            p1.Y = -82.451598f;
+            Point p2 = new Point();
+            p2.X = 27.207680f;
+            p2.Y = -82.451278f;
+            Point p3 = new Point();
+            p3.X = 27.207965f;
+            p3.Y = -82.451158f;
+            Point p4 = new Point();
+            p4.X = 27.208071f;
+            p4.Y = -82.451475f;
+            List<Point> points = new List<Point>();
+            points.Add(p1);
+            points.Add(p2);
+            points.Add(p3);
+            points.Add(p4);
+
+            Point gps = new Point();
+            gps.X = 27.207865f;
+            gps.Y = -82.451644f;
+
+            bool inside = Point.IsPointInPolygon(gps, points.ToArray());
+            Console.WriteLine(inside);
+
         }
     }
 }
