5e58bfd1e259024facddd2c2ece3b4bc7776cdfe
Tim Hanewich <tahanewich@live.com>
2022-07-18 16:28:44 -0400

    Added decel vest 2

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ strobe_test = 1
 record_processed = 2
 record_raw = 3
 strobe_solid = 4
+decel_vest2 = 5
 
 # RUN MODE
-EXECUTION_MODE = record_raw
+EXECUTION_MODE = decel_vest2
 
 if EXECUTION_MODE == decel_vest:
     import decel_vest
@@ -18,5 +19,7 @@ elif EXECUTION_MODE == record_raw:
     import record_raw
 elif EXECUTION_MODE == strobe_solid:
     import strobe_solid
+elif EXECUTION_MODE == decel_vest2:
+    import decel_vest2
 else:
     print("Did not understand execution mode '" + str(EXECUTION_MODE) + "'")
