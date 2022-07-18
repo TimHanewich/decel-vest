3ce739def0048a08046906383c389788b277b1d1
Tim Hanewich <tahanewich@live.com>
2022-07-18 18:03:41 -0400

    Got rid of old decel vest that uses GPS and renamed the #2 to the normal

--- src/decel_vest.py
+++ src/decel_vest.py
@@ -38,7 +38,6 @@ _thread.start_new_thread(strobe_controller.continuous_strobe, ())
 
 # create the variables that we will use
 gps = gps_driver.gps_driver()
-sc = speed_controller.speed_controller()
 strobe_calc = strobe_calculator.strobe_calculator()
 
 # set up the GPS driver
@@ -47,33 +46,20 @@ gps.setup()
 print("GPS driver set up!")
 
 # print the headers
-print("fix_speed_lat_lon_sats_hz", False)
+print("fix_speed_hz", False)
 
 while True:
     try:
 
-        # get telemetry from the gps driver
-        #print("Getting telemetry")
-        tele = gps.get_telemetry(3000)
-        #print("Telemetry received!")
+        # get the next sentence
+        line = gps.collect_nmea_sentence()
 
-        if tele != None:
-            #print("Telemetry was not None!")
-            if tele.fixed != None and tele.latitude != None and tele.longitude != None:
-                #print("We have the necessary data...")
-                
-                # calculate  the speed
-                #print("Calculating speed...")
-                sc.ingest(tele.fixed, tele.latitude, tele.longitude) #ingest the data to get the speed
-                #print("sc ingested.")
-        
-                # if we have the speed (which we should, try to calculate the strobe speed)
-                if sc.speed_mph != None:
-                    #print("We have an MPH!")
-
-                    # calculate the appropriate hertz of the light
-                    #print("Going to calculate hz")
-                    hz = strobe_calc.ingest(tele.fixed, sc.speed_mph)
+        if line != None:
+            if nmea.is_gprmc(line):
+                fixed = nmea.get_gprmc_fixed(line)
+                speed = nmea.get_speed_mph(line)
+                if fixed != None and speed != None:
+                    hz = strobe_calc.ingest(fixed, speed)
                     if hz != None:
                         #print("Hertz was something: " + str(hz))
                         strobe_controller.set_hertz(hz)
@@ -83,8 +69,9 @@ while True:
                         strobe_controller.mute()
 
                     # log the data
-                    print(str(tele.fixed) + "_" + str(sc.speed_mph) + "_" + str(tele.latitude) + "_" + str(tele.longitude) + "_" + str(tele.satellites) + "_" + str(hz))
-    
+                    print(str(fixed) + "_" + str(speed) + "_" + str(hz))
+
+
     except Exception as e:
         print("GOT AN ERROR!")
         print("Critical error! Msg: " + str(e))
--- src/main.py
+++ src/main.py
@@ -4,10 +4,9 @@ strobe_test = 1
 record_processed = 2
 record_raw = 3
 strobe_solid = 4
-decel_vest2 = 5
 
 # RUN MODE
-EXECUTION_MODE = decel_vest2
+EXECUTION_MODE = decel_vest
 
 if EXECUTION_MODE == decel_vest:
     import decel_vest
@@ -19,7 +18,5 @@ elif EXECUTION_MODE == record_raw:
     import record_raw
 elif EXECUTION_MODE == strobe_solid:
     import strobe_solid
-elif EXECUTION_MODE == decel_vest2:
-    import decel_vest2
 else:
     print("Did not understand execution mode '" + str(EXECUTION_MODE) + "'")
