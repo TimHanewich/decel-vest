5842991f9cdcaaca732c92c70269a844c8d66d69
Tim Hanewich <tahanewich@live.com>
2022-07-19 15:53:11 -0400

    Separated out the act on functions with the main while loop

--- src/decel_vest.py
+++ src/decel_vest.py
@@ -49,6 +49,27 @@ print("GPS driver set up!")
 # print the headers
 print("fix_speed_hz", False)
 
+def act_on_speed(fixed:int, speed_mph:float):
+
+    global strobe_calc
+
+    hz = strobe_calc.ingest(fixed, speed)
+
+    # set the hz that we have
+    if hz == None: # nothing was provided, so mute the lights (show nothing)
+        strobe_controller.mute()
+    elif type(hz) == float: # a hz was provided, so make sure we are in hz mode, set the hz, and then show it!
+        strobe_controller.set_mode(strobe_controller.mode_hz) #set to hz mode
+        strobe_controller.set_hertz(hz)
+        strobe_controller.unmute()
+    elif type(hz) == str: # a string value was provided, signifying we are following a standard pattern
+        if hz == "polygon": # we are within a polygon, so set the mode to pit lane mode
+            strobe_controller.set_mode(strobe_controller.mode_pit) #set to pit lane mode
+
+def act_on_gps(latitude:float, longitude:float):
+    global strobe_calc
+    strobe_calc.ingest_gps(tele.latitude, tele.longitude)
+
 while True:
     try:
 
@@ -61,26 +82,13 @@ while True:
                 fixed = nmea.get_gprmc_fixed(line)
                 speed = nmea.get_speed_mph(line)
                 if fixed != None and speed != None:
-                    hz = strobe_calc.ingest(fixed, speed)
-
-                    # set the hz that we have
-                    if hz == None: # nothing was provided, so mute the lights (show nothing)
-                        strobe_controller.mute()
-                    elif type(hz) == float: # a hz was provided, so make sure we are in hz mode, set the hz, and then show it!
-                        strobe_controller.set_mode(strobe_controller.mode_hz) #set to hz mode
-                        strobe_controller.set_hertz(hz)
-                        strobe_controller.unmute()
-                    elif type(hz) == str: # a string value was provided, signifying we are following a standard pattern
-                        if hz == "polygon": # we are within a polygon, so set the mode to pit lane mode
-                            strobe_controller.set_mode(strobe_controller.mode_pit) #set to pit lane mode
-                
-                    # log the data
+                    act_on_speed(fixed, speed)
                     print(str(fixed) + "_" + str(speed) + "_" + str(hz))
             elif nmea.is_gpgga(line):
                 tele = nmea.parse(line)
                 if tele != None:
                     if tele.latitude != None and tele.longitude != None:
-                        strobe_calc.ingest_gps(tele.latitude, tele.longitude)
+                        act_on_gps(tele.latitude, tele.longitude)
                         print("GPS logged: " + str(tele.latitude) + ", " + str(tele.longitude))
 
 
