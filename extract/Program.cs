0e79e44b835c4c671140007cf82c78b128e7ad99
Tim Hanewich <tahanewich@live.com>
2022-07-18 23:48:11 -0400

    Recreated polygon and wrote to src

--- extract/Program.cs
+++ extract/Program.cs
@@ -11,21 +11,7 @@ namespace extract
         public static void Main(string[] args)
         {
 
-            List<Point> points = new List<Point>();
-            points.Add(Point.Parse("27.165232, -82.458301"));
-            points.Add(Point.Parse("27.165232, -82.458301"));
-            points.Add(Point.Parse("27.163361, -82.458677"));
-            points.Add(Point.Parse("27.163361, -82.458677"));
-            points.Add(Point.Parse("27.159291, -82.459620"));
-            points.Add(Point.Parse("27.159084, -82.459645"));
-            points.Add(Point.Parse("27.157131, -82.459588"));
-            points.Add(Point.Parse("27.155394, -82.460143"));
-            points.Add(Point.Parse("27.154569, -82.461009"));
-            points.Add(Point.Parse("27.155135, -82.461520"));
-            points.Add(Point.Parse("27.167604, -82.461492"));
-            
-
-            Console.WriteLine(JsonSerializer.Serialize(points.ToArray()));
+            CompilePolygons();
 
         }
 
