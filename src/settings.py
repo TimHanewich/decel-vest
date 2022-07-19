e384b5b5ca52d849b8421e106c8878a06049f299
Tim Hanewich <tahanewich@live.com>
2022-07-19 11:12:13 -0400

    Lifted min and max deceleration that is used to calculate where we are in the deceleration window, and thus a strobe hz

--- src/settings.py
+++ src/settings.py
@@ -16,5 +16,5 @@ min_decel_trigger = 1.0 # the minimum deceleration that has to occur for it to b
 min_decel_sustained = 0.3 #when deceleration is first detected, this is the minimum deceleration that has to be sustained in order for deceleration to be sustained. The moment deceleration drops below this, we move to the "awaiting deceleration" mode.
 
 # Deceleration settings for calculating hz - these are used to calcualte where we are in the range and thus calculate a hz for the strobe
-min_decel_strobe = 0.3
-max_decel_strobe = 3.0
+min_decel_strobe = 0.8
+max_decel_strobe = 4.2
